e0433af2a3e6ccdb6f0d16988a0b58d3a0cd7f85
Koi Den <prostobydet@gmail.com>
2023-11-20 10:09:23 +0300

    trycatvh and dic

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,4 +1,107 @@
-﻿using OOP.ForestExample;
+﻿try
+{
+    int x = 5;
+    int y = x / 0;
+    Console.WriteLine($"Результат: {y}");
+    if(y < 0)
+    {
+        throw new Exception("");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Блок finally");
+}
+Console.WriteLine("Конец программы");
+
+//Dictionary<int, string> people = new 
+//Dictionary<int, string>();
+
+//people = new Dictionary<int, string>()
+//{
+//    { 5, "Tom"},
+//    { 3, "Sam"},
+//    { 11, "Bob"}
+//};
+//people[1] = "Bob";
+//people.Add(2, "dfdf");
+//people.Clear();
+//people.ContainsKey(1);
+//people.Remove(1);
+//foreach (var person in people)
+//{
+//    Console.WriteLine($"key: " +
+//        $"{person.Key}  value: " +
+//        $"{person.Value}");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using OOP.ForestExample;
+//Console.WriteLine("Гномья сортировка");
+//Console.Write("Введите элементы массива: ");
+//var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+//var array = new int[parts.Length];
+//for (int i = 0; i < parts.Length; i++)
+//{
+//    array[i] = Convert.ToInt32(parts[i]);
+//}
+
+//Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", GnomeSort(array)));
+
+//Console.ReadLine();
+
+//static void Swap(ref int item1, ref int item2)
+//{
+//    var temp = item1;
+//    item1 = item2;
+//    item2 = temp;
+//}
+
+////Гномья сортировка
+//static int[] GnomeSort(int[] unsortedArray)
+//{
+//    var index = 1;
+//    var nextIndex = index + 1;
+
+//    while (index < unsortedArray.Length)
+//    {
+//        if (unsortedArray[index - 1] < unsortedArray[index])
+//        {
+//            index = nextIndex;
+//            nextIndex++;
+//        }
+//        else
+//        {
+//            Swap(ref unsortedArray[index - 1], ref unsortedArray[index]);
+//            index--;
+//            if (index == 0)
+//            {
+//                index = nextIndex;
+//                nextIndex++;
+//            }
+//        }
+//    }
+
+//    return unsortedArray;
+//}
+
 
 // вывод ёжиков
 //Forest forest = new Forest();
@@ -64,41 +167,41 @@
 //    }
 //}
 
-Transport transport = new Car();
-List<Transport> lst = new List<Transport>();
-lst.Add(new Car());
-lst.Add(new Ship());
-lst.Add(new Aircraft());
-foreach(var i in lst)
-{
-    i.Move();
-}
-abstract class Transport
-{
-    public abstract void Move();
-}
-// класс корабля
-class Ship : Transport
-{
-    // мы должны реализовать все абстрактные методы и свойства базового класса
-    public override void Move()
-    {
-        Console.WriteLine("Корабль плывет");
-    }
-}
-// класс самолета
-class Aircraft : Transport
-{
-    public override void Move()
-    {
-        Console.WriteLine("Самолет летит");
-    }
-}
-// класс машины
-class Car : Transport
-{
-    public override void Move()
-    {
-        Console.WriteLine("Машина едет");
-    }
-}
+//Transport transport = new Car();
+//List<Transport> lst = new List<Transport>();
+//lst.Add(new Car());
+//lst.Add(new Ship());
+//lst.Add(new Aircraft());
+//foreach(var i in lst)
+//{
+//    i.Move();
+//}
+//abstract class Transport
+//{
+//    public abstract void Move();
+//}
+//// класс корабля
+//class Ship : Transport
+//{
+//    // мы должны реализовать все абстрактные методы и свойства базового класса
+//    public override void Move()
+//    {
+//        Console.WriteLine("Корабль плывет");
+//    }
+//}
+//// класс самолета
+//class Aircraft : Transport
+//{
+//    public override void Move()
+//    {
+//        Console.WriteLine("Самолет летит");
+//    }
+//}
+//// класс машины
+//class Car : Transport
+//{
+//    public override void Move()
+//    {
+//        Console.WriteLine("Машина едет");
+//    }
+//}
